98a3bd7273c86fe1c79069f13a76f333a20c38e6
RemoteJava <158183671+RemoteJava@users.noreply.github.com>
2026-01-05 12:30:13 -0600

    Add DragonLibrary component to App

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DragonLibrary from './components/DragonLibrary'; // <--- NEW IMPORT
+
+function App() {
+  return (
+    <div className="app-container"> 
+      <DragonLibrary /> 
+    </div>
+  );
+}
+
+export default App;
